152a8127877b4b0684f07585fe836e42466027a7
姚文强 <yaowenqiang111@163.com>
2022-07-04 23:42:33 +0800

    add more prop demo and custom eevent demo

--- vue-cli/src/UserDetail.vue
+++ vue-cli/src/UserDetail.vue
@@ -2,13 +2,36 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{ name }}</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['name']
+        props: {
+            // name: [String, Array]
+            // name: [String]
+            name: {
+                // type: String,
+                type: Object,
+                required: true,
+                default: function () {
+                    return {
+                        name: 'Max'
+                    }
+                }
+            }
+        },
+        methods: {
+            switchName() {
+                return this.name.split('').reverse().join('');
+            },
+            resetName() {
+                // this.name = 'Max';
+                this.$emit('nameWasReset', this.name)
+            }
+        }
     }
 </script>
 
